a9850b583066df1fe6839ea3b3fa044e635c9b9a
Fantom <ivanneporadnyj@gmail.com>
2024-07-27 18:01:41 +0300

    Initial Commit

---
+++ CNMAApp2/CNMAApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  CNMAApp2App.swift
+//  CNMAApp2
+//
+//  Created by Иван Непорадный on 27.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CNMAApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CNMAApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CNMAApp2
+//
+//  Created by Иван Непорадный on 27.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
